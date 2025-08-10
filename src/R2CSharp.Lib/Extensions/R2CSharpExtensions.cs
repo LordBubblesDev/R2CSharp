69d302377e9e4ff8eb5a31d11d5420e3b404e97a
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2025-08-10 06:40:42 +0200

    Properly catch exceptions in R2CSharpExtensions.cs

--- src/R2CSharp.Lib/Extensions/R2CSharpExtensions.cs
+++ src/R2CSharp.Lib/Extensions/R2CSharpExtensions.cs
@@ -1,9 +1,11 @@
 using Avalonia;
 using Avalonia.Markup.Xaml.Styling;
-using Avalonia.Styling;
 using Projektanker.Icons.Avalonia;
 using Projektanker.Icons.Avalonia.FontAwesome;
 using R2CSharp.Lib.Components;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
 
 namespace R2CSharp.Lib.Extensions;
 
@@ -19,19 +21,16 @@ public static class R2CSharpExtensions
     /// <returns>The AppBuilder instance for method chaining</returns>
     public static AppBuilder UseR2CSharp(this AppBuilder appBuilder)
     {
-        return appBuilder.AfterSetup(_ =>
-        {
+        return appBuilder.AfterSetup(_ => {
             // Ensure resources are loaded before any views are created
-            if (Application.Current?.Resources is not null)
+            if (Application.Current?.Resources is null) return;
+            var themeUri = new Uri("avares://R2CSharp.Lib/Themes/R2CSharpTheme.axaml");
+            var themeResource = new ResourceInclude(themeUri)
             {
-                var themeUri = new Uri("avares://R2CSharp.Lib/Themes/R2CSharpTheme.axaml");
-                var themeResource = new ResourceInclude(themeUri)
-                {
-                    Source = themeUri
-                };
+                Source = themeUri
+            };
                 
-                Application.Current.Resources.MergedDictionaries.Add(themeResource);
-            }
+            Application.Current.Resources.MergedDictionaries.Add(themeResource);
         });
     }
     
@@ -48,9 +47,10 @@ public static class R2CSharpExtensions
             IconProvider.Current
                 .Register(new FontAwesomeIconProvider(FontAwesomeJsonStreamProvider.Instance));
         }
-        catch (Exception)
-        {
-            // FontAwesome already registered, ignore
+        catch (Exception ex) {
+            Console.WriteLine("You most likely tried to use this library in a context where FontAwesome is already registered.");
+            Console.WriteLine("If that is the case, consider initializing with UseR2CSharp() instead.");
+            Console.WriteLine(ex);
         }
         
         // Then load theme resources
